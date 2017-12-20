d9ce0a27aff7ea72bb922cfe4974ba16db0de20b
jay-jain <jainj61@lsus.edu>
2017-12-20 11:44:09 -0600

    Tried to fix rendering of static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,32 @@
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationControllerhelp
   def home
   end
 
   def help
+  	#render 'static_pages/help'
   end
+
+  def about
+  end
+
+  # def index
+  # end
+
+  # def create
+  # end
+
+  # def new
+  # end
+
+  # def edit
+  # end
+
+  # def show
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  #get 'static_pages/home'
 
   get 'static_pages/help'
 
   resources :microposts
   resources :users
   resources :static_pages
-  #resources :startup
-  #resources :layouts
-  #get 'startup/index'
+  # resources :startup
+   resources :layouts
+  # get 'startup/index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   root 'static_pages#home'
+
+  #get 'static_pages/help', to: 'static_pages#help', as: :help
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
