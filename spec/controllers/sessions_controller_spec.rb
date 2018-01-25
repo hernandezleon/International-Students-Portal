eb572916304080700e6eef4e5d98f0886cb6746c
Unknown <jainj61@lsus.edu>
2018-01-25 10:08:59 -0600

    Fixed syntax in sessions controller rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 # RSpec.describe SessionsController, type: :controller do
 
@@ -16,4 +16,4 @@ require 'rails_helper'
 #     end
 #   end
 
-end
+# end
