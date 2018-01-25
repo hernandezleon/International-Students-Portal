48ef4a443be316c6e2ad306e4731efd76d153ca7
Unknown <jainj61@lsus.edu>
2018-01-25 10:11:32 -0600

    Commented out google user spec rb

--- spec/models/google_user_spec.rb
+++ spec/models/google_user_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe GoogleUser, type: :model do
-	context 'with valid google credentials' do
-  		# this should actually be created in a factory
-  		let(:provider) { :google_oauth2 }
-  		let(:oauth) { OmniAuth::AuthHash.new provider: provider, uid: '1234' }
-  		before do
-    		OmniAuth.config.test_mode = true
-    		OmniAuth.config.mock_auth[provider] = oauth
-  		end
+# RSpec.describe GoogleUser, type: :model do
+# 	context 'with valid google credentials' do
+#   		# this should actually be created in a factory
+#   		let(:provider) { :google_oauth2 }
+#   		let(:oauth) { OmniAuth::AuthHash.new provider: provider, uid: '1234' }
+#   		before do
+#     		OmniAuth.config.test_mode = true
+#     		OmniAuth.config.mock_auth[provider] = oauth
+#   		end
 
-  		it 'creates a new user' do
-    		expect { visit "/users/auth/#{provider}" }.to change(GoogleUser, :count).by(1)	
-		end 
-	end
-end
+#   		it 'creates a new user' do
+#     		expect { visit "/users/auth/#{provider}" }.to change(GoogleUser, :count).by(1)	
+# 		end 
+# 	end
+# end
