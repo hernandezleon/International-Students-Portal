d6fb942d883399347cd1f42ffc120ff3a7daa988
Unknown <jainj61@lsus.edu>
2018-02-05 18:26:03 -0600

    Messed up the login system
    Need to find a way to route the User model to the navbar dropdown and landing page jumbotron.
    
    Also redirect admin users to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   require 'omniauth-google-oauth2'
   get 'sessions/create'
 
   get 'sessions/destroy'
+
+  get 'sessions/user'
 
   get 'home/show'
 
@@ -76,11 +80,11 @@ Rails.application.routes.draw do
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('startup')
     get 'signout', to: 'sessions#destroy', as: 'signout'
-
     resources :sessions, only: [:create, :destroy]
     resource :home, only: [:show]
 
-    #root to: "home#show"
+    post ':controller(/:action(/:id(.:format)))'
+    get ':controller(/:action(/:id(.:format)))'
   
  
 
