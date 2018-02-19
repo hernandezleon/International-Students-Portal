31943777d26cbccd548661c72ed57340539982f8
Unknown <jainj61@lsus.edu>
2018-02-19 08:23:50 -0600

    Up to date

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   resources :google_user
+  resource :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
