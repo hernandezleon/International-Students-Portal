6f1da88e93d690c3f385c9329c40c00648a621aa
Jay <jainj61@lsus.edu>
2018-02-19 20:21:56 -0600

    Updated _header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,7 @@ Rails.application.routes.draw do
   
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('startup')
-    get 'signout', to: 'sessions#destroy', as: 'signout'
+    # get 'signout', to: 'sessions#destroy', as: 'signout'
     resources :sessions, only: [:create, :destroy]
     resource :home, only: [:show]
 
