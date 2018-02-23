349803ee0ffb90642f4875bda8aec1d60b3f426a
jay-jain <jainj61@lsus.edu>
2018-02-23 17:27:21 -0600

    Implemented mailing system and user deletion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+
+
+  # Local host email configuration
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+ 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
+
+  config.action_mailer.default_url_options = {:host => 'international-students-portal.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "127.0.0.1",
+  :port    => 25,
+  :domain  => 'international-students-portal.herokuapp.com'
+  }  
 end
