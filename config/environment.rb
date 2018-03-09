261c8da609aaf44de259b4aabc3757c49da2a275
Unknown <jainj61@lsus.edu>
2018-03-09 15:02:26 -0600

    Added openssl require statement

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require 'tlsmail'
+require 'openssl'
 
 # Initialize the Rails application.
 Rails.application.initialize!
