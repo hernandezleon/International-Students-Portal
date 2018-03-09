946c442153c86c93526a9918241425d7651ae20f
Unknown <jainj61@lsus.edu>
2018-03-09 12:54:41 -0600

    Added email server settings to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,22 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'tlsmail'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+	:enable_starttls_auto => true,  
+	:address => "smtp.gmail.com",
+  	:port    => 25,
+  	:tls => true,
+  	:domain => 'gmail.com',
+  	:authentication => :plain,
+  	:user_name => ENV["GMAIL_USERNAME"],
+  	:password => ENV["GMAIL_PASSWORD"]
+}
