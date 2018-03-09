5c5ab9335bfaaec8cd138024fa4b8990646a87f5
Unknown <jainj61@lsus.edu>
2018-03-09 17:19:14 -0600

    removed require tlsmail and openssl

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'tlsmail'
-require 'openssl'
+# require 'tlsmail'
+# require 'openssl'
 
 # Initialize the Rails application.
 Rails.application.initialize!
